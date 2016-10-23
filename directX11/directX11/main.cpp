540c0cfb88dded7eeac759e7d370e0d4e992af31
zhangbiran <534901813@qq.com>
2016-10-23 23:35:54 +0800

    directX11

--- directX11/directX11/main.cpp
+++ directX11/directX11/main.cpp
@@ -1,6 +1,171 @@
-#include <iostream>
 
-int main(void)
+
+/*
+Beginning DirectX 11 Game Programming
+By Allen Sherrod and Wendy Jones
+*/
+
+/*
+#include<Windows.h>
+#include<memory>
+
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
 {
+	UNREFERENCED_PARAMETER(prevInstance);
+	UNREFERENCED_PARAMETER(cmdLine);
+
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = "DX11BookWindowClass";
+
+	if (!RegisterClassEx(&wndClass))
+		return -1;
+
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindowA("DX11BookWindowClass", "Blank Direct3D 11 Window", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
+		NULL, NULL, hInstance, NULL);
+
+	if (!hwnd)
+		return -1;
+
+	ShowWindow(hwnd, cmdShow);
+
+	MSG msg = { 0 };
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hDC = BeginPaint(hwnd, &paintStruct);
+		EndPaint(hwnd, &paintStruct);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}*/
+
+
+#include <windows.h>
+#include <cstdio>
+LRESULT CALLBACK WindowProc(
+	HWND hwnd,      // handle to window
+	UINT uMsg,      // message identifier
+	WPARAM wParam,  // first message parameter
+	LPARAM lParam   // second message parameter
+	);
+
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,      // handle to current instance
+	HINSTANCE hPrevInstance,  // handle to previous instance
+	LPSTR lpCmdLine,          // command line
+	int nCmdShow              // show state
+	)
+{
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_VREDRAW | CS_HREDRAW;
+	wndClass.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WindowProc;
+	wndClass.lpszClassName = "directX11";
+	wndClass.lpszMenuName = NULL;
+
+	ATOM atom = RegisterClassEx(&wndClass);
+	if (atom == 0)
+	{
+		return -1;
+	}
+
+	HWND hwnd = CreateWindow("directX11", "directX11", WS_OVERLAPPEDWINDOW, 0, 0, 640, 480, NULL, NULL, hInstance, NULL);
+	if (NULL == hwnd)
+	{
+		char buf[100] = { 0 };
+		sprintf_s(buf, "%d", GetLastError());
+		MessageBox(NULL, buf, "error", MB_OK);
+	}
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+
+	return static_cast<int>(msg.wParam);
+}
+
+
+LRESULT CALLBACK WindowProc(
+	HWND hwnd,      // handle to window
+	UINT uMsg,      // message identifier
+	WPARAM wParam,  // first message parameter
+	LPARAM lParam   // second message parameter
+	)
+{
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (uMsg)
+	{
+	case WM_PAINT:
+		hDC = BeginPaint(hwnd, &paintStruct);
+		LineTo(hDC, 100, 100);
+		EndPaint(hwnd, &paintStruct);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
 	return 0;
 }
