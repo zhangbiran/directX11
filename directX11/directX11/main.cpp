e84deaaa3adb443cb61c283b5f56bc2a8b636424
zhangbiran <534901813@qq.com>
2016-10-25 00:03:28 +0800

    directX11

--- directX11/directX11/main.cpp
+++ directX11/directX11/main.cpp
@@ -1,90 +1,13 @@
-
-
-/*
-Beginning DirectX 11 Game Programming
-By Allen Sherrod and Wendy Jones
-*/
-
-/*
-#include<Windows.h>
-#include<memory>
-
-
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
-{
-	UNREFERENCED_PARAMETER(prevInstance);
-	UNREFERENCED_PARAMETER(cmdLine);
-
-	WNDCLASSEX wndClass = { 0 };
-	wndClass.cbSize = sizeof(WNDCLASSEX);
-	wndClass.style = CS_HREDRAW | CS_VREDRAW;
-	wndClass.lpfnWndProc = WndProc;
-	wndClass.hInstance = hInstance;
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wndClass.lpszMenuName = NULL;
-	wndClass.lpszClassName = "DX11BookWindowClass";
-
-	if (!RegisterClassEx(&wndClass))
-		return -1;
-
-	RECT rc = { 0, 0, 640, 480 };
-	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-
-	HWND hwnd = CreateWindowA("DX11BookWindowClass", "Blank Direct3D 11 Window", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
-		NULL, NULL, hInstance, NULL);
-
-	if (!hwnd)
-		return -1;
-
-	ShowWindow(hwnd, cmdShow);
-
-	MSG msg = { 0 };
-
-	while (msg.message != WM_QUIT)
-	{
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
-
-	return static_cast<int>(msg.wParam);
-}
-
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	PAINTSTRUCT paintStruct;
-	HDC hDC;
-
-	switch (message)
-	{
-	case WM_PAINT:
-		hDC = BeginPaint(hwnd, &paintStruct);
-		EndPaint(hwnd, &paintStruct);
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	default:
-		return DefWindowProc(hwnd, message, wParam, lParam);
-	}
-
-	return 0;
-}*/
-
-
+#include <D3D11.h>
+#include <DXGI.h>
 #include <windows.h>
 #include <cstdio>
+
+ID3D11Device * IDevice = NULL;
+IDXGISwapChain * ISwapChain = NULL;
+ID3D11DeviceContext * IContext = NULL;
+
+
 LRESULT CALLBACK WindowProc(
 	HWND hwnd,      // handle to window
 	UINT uMsg,      // message identifier
@@ -125,6 +48,46 @@ int WINAPI WinMain(
 	}
 
 	ShowWindow(hwnd, SW_SHOW);
+
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	desc.BufferCount = 1;
+	desc.OutputWindow = hwnd;
+	desc.Windowed = true;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.BufferDesc.Width = 480;
+	desc.BufferDesc.Height = 640;
+	desc.BufferDesc.RefreshRate.Numerator = 60;
+	desc.BufferDesc.RefreshRate.Denominator = 1;
+	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	desc.SampleDesc.Count = 1;
+	desc.SampleDesc.Quality = 0;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.BufferCount = 1;
+	desc.OutputWindow = hwnd;
+	desc.Windowed = true;
+
+	HRESULT ret = D3D11CreateDeviceAndSwapChain(
+		NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		0,
+		NULL,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		&ISwapChain,
+		&IDevice,
+		NULL,
+		&IContext);
+
+	if (S_OK == ret)
+	{
+		MessageBox(NULL, "D3D11CreateDeviceAndSwapChain", "success", MB_OK);
+	}
+
+
+
 
 	MSG msg = { 0 };
 	while (msg.message != WM_QUIT)
